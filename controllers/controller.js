5cc0be607bf42c29eda606ce52e0318f385e633a
sanjaymenon <sanjayzzzz@gmail.com>
2016-11-29 02:22:56 +0530

    removing comments, adding bidirectional relation logic

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,11 +3,9 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http) {
 
-  //console.log("Hello");
 //Function to get all the realtions and redraw the diagram.
   var refresh = function(){
 	  	$http.get('/relationList').success(function(response){
-	  		//console.log("Got the data");
 	  		$scope.relationList = response;
 	  		$scope.relation={};
 	  		$scope.currentRelation={};
@@ -17,9 +15,6 @@ myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http) {
   //function to create the network diagram
   var draw = function(){
 
-  	for (var i = 0; i < $scope.relationList.length; i++) {
-  		//console.log($scope.relationList[i]);
-  	}
   	var edges=[];
   	var unique = {};
 	var distinct = [];
@@ -39,7 +34,7 @@ myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http) {
 	 	 edges.push(myEdge);
 
 	}
-	console.dir(edges);
+
 	var nodes=[];
 	for (var i = 0; i < distinct.length; i++) {
 		var myObj ={};
@@ -47,7 +42,7 @@ myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http) {
 		myObj.label=distinct[i]
 		nodes.push(myObj);
 	}
-	//console.log(nodes);
+
 
 	
 
@@ -67,27 +62,44 @@ myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http) {
   	// Function which calls services to first check if the relation exist , then depending on that update the relation or create a new relation
 
   	$scope.addNewRelation = function(){
-  		console.log($scope.relation);
+
 
   		$http.get('/getRelation',{params:{parent: $scope.relation.parent,child:$scope.relation.child}}).success(function(response){
-	  		//console.log("back in single record req");
-	  		//console.log(response);
+
 	  		$scope.currentRelation = response;
-	  		//console.log($scope.currentRelation);
+
 	  		if($scope.currentRelation!=null){
 		  		if($scope.currentRelation.parent == $scope.relation.parent && $scope.currentRelation.child == $scope.relation.child){
-		  			//console.log('update');
+
 		  			$http.put('/relationList/'+$scope.currentRelation._id,$scope.relation).success(function(response){
-		  				//console.log(response);
+
 		  				refresh();
 		  			})
 		  		}
 	  		}
 	  		else{
-	  			$http.post('/relationList',$scope.relation).success(function(response){
-	  				//console.log(response);
-	  				refresh();
+	  				
+	  			$http.get('/getRelation',{params:{parent: $scope.relation.child, child:$scope.relation.parent}}).success(function(response){
+	  				$scope.currentRelation = response;
+	  				if($scope.currentRelation!=null){
+				  		if($scope.currentRelation.parent == $scope.relation.child && $scope.currentRelation.child == $scope.relation.parent){
+				  			var reverseRelation ={};
+				  			reverseRelation.parent = $scope.relation.child;
+				  			reverseRelation.child = $scope.relation.parent;
+				  			reverseRelation.relation = $scope.relation.relation;
+				  			$http.put('/relationList/'+$scope.currentRelation._id,$scope.relation).success(function(response){
+				  				refresh();
+				  			})
+				  		}
+			  		}
+			  		else{	
+				  			$http.post('/relationList',$scope.relation).success(function(response){
+				  				refresh();
+				  			});
+				  	}
+
 	  			});
+	  			
 	  		}
 	  	});
 
--- server.js
+++ server.js
@@ -11,16 +11,13 @@ app.use(bodyParser.json());
 
 //service to  retrieve all the relation
 app.get('/relationList', function(req,res){
-	//console.log('i got a get request')
 	db.relationlist.find(function(err,docs){
-		//console.log(docs);
 		res.json(docs);
 	});	
 });
 
 //service to create a new relation
 app.post('/relationList',function(req,res){
-	//console.log(req.body);
 	var parentCharacter ="";
 	var childCharacter ="";
 	db.relationlist.insert(req.body,function(err,doc){
@@ -30,21 +27,14 @@ app.post('/relationList',function(req,res){
 
 //service to get one relation
 app.get('/getRelation', function(req,res){
-	//console.log('single record req');
-	//console.log(req.query.parent);
-	//console.log(req.query.child);
 	db.relationlist.findOne({parent: req.query.parent, child: req.query.child},function(err,docs){
-		//console.log(docs);
 		res.json(docs);
 	});	
 });
 
 //service to update the relation
 app.put('/relationList/:id',function(req,res){
-	//console.log("insideupdate")
 	var id = req.params.id;
-	//console.log(id);
-	//console.log(req.body.parent);
 	db.relationlist.findAndModify({
 		query:{_id:mongojs.ObjectId(id)},
 		update: {$set:{parent:req.body.parent, child:req.body.child, relation:req.body.relation}},
